88af02f314361c631a143ef64ab117bb74786276
trevor <trevor.laing@gmail.com>
2026-08-10 13:04:16 -0400

    11th commit commentary added

--- netcat.py
+++ netcat.py
@@ -1,3 +1,10 @@
+# Simple netcat-like tool supporting:
+#   - connect/send mode
+#   - listen mode with command shell
+#   - one-shot execute mode
+#   - file upload mode
+#
+# Usage examples are available in the argparse epilog below.
 import argparse
 import socket
 import shlex
@@ -6,6 +13,7 @@ import sys
 import textwrap
 import threading
 
+# Run a shell command and return the decoded output.
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
@@ -17,16 +25,19 @@ class NetCat:
     def __init__(self, args, buffer=None):
         self.args = args
         self.buffer = buffer
+        # Create a TCP socket and allow address reuse.
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def run(self):
+        # Decide whether to listen as a server or connect as a client.
         if self.args.listen:
             self.listen()
         else:
             self.send()
 
     def send(self):
+        # Connect to the remote target and optionally send initial input.
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
             self.socket.send(self.buffer)
@@ -34,35 +45,53 @@ class NetCat:
             while True:
                 recv_len = 1
                 response = ''
+                # Read from the remote socket until no more data arrives.
                 while recv_len:
                     data = self.socket.recv(4096)
                     recv_len = len(data)
+                    if not recv_len:
+                        break
                     response += data.decode()
                     if recv_len < 4096:
                         break
                 if response:
-                    print(response, end=' ')
+                    print(response, end='')
                     buffer = input('> ')
                     buffer += '\n'
                     self.socket.send(buffer.encode())
+                else:
+                    break
         except KeyboardInterrupt:
             print('User terminated.')
+        finally:
             self.socket.close()
             sys.exit()
 
     def listen(self):
+        # Bind to the local address and wait for incoming client connections.
         self.socket.bind((self.args.target, self.args.port))
         self.socket.listen(5)
-        while True:
-            client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
-            client_thread.start()
+        try:
+            while True:
+                client_socket, _ = self.socket.accept()
+                client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+                client_thread.daemon = True
+                client_thread.start()
+        except KeyboardInterrupt:
+            print('Shutting down.')
+        finally:
+            self.socket.close()
+            sys.exit()
 
     def handle(self, client_socket):
+        # Handle a connected client based on the selected mode.
         if self.args.execute:
             output = execute(self.args.execute)
-            client_socket.send(output.encode())
+            if output:
+                client_socket.send(output.encode())
+            client_socket.close()
         elif self.args.upload:
+            # Receive a file upload and save it locally.
             file_buffer = b''
             while True:
                 data = client_socket.recv(4096)
@@ -70,16 +99,17 @@ class NetCat:
                     break
                 file_buffer += data
 
-
             with open(self.args.upload, 'wb') as f:
                 f.write(file_buffer)
             message = f'Saved file {self.args.upload}'
             client_socket.send(message.encode())
+            client_socket.close()
 
         elif self.args.command:
+            # Provide an interactive command shell to the client.
             cmd_buffer = b''
-            while True:
-                try:
+            try:
+                while True:
                     client_socket.send(b'BHP: #> ')
                     while b'\n' not in cmd_buffer:
                         data = client_socket.recv(64)
@@ -88,14 +118,21 @@ class NetCat:
                             return
                         cmd_buffer += data
 
-                    response = execute(cmd_buffer.decode())
+                    command = cmd_buffer.decode().strip()
+                    cmd_buffer = b''
+                    if not command:
+                        continue
+                    if command.lower() in ('exit', 'quit'):
+                        client_socket.send(b'Bye!\n')
+                        client_socket.close()
+                        return
+
+                    response = execute(command)
                     if response:
                         client_socket.send(response.encode())
-                    cmd_buffer = b''
-                except Exception as e:
-                    print(f'Server killed {e}')
-                    client_socket.close()
-                    return
+            except Exception as e:
+                print(f'Server killed {e}')
+                client_socket.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
